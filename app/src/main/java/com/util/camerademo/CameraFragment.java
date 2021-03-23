bc0d7e5d76a4592f5111d7be420f864d70559f82
zhou.penghui <zhou516371383@163.com>
2021-03-23 18:21:00 +0800

    完成了相机调用

--- app/src/main/java/com/util/camerademo/CameraFragment.java
+++ app/src/main/java/com/util/camerademo/CameraFragment.java
@@ -2,63 +2,74 @@ package com.util.camerademo;
 
 import android.os.Bundle;
 
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link CameraFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * 相机控制Fragment
+ * create By zhph777@gmail.com
  */
 public class CameraFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CameraFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CameraFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CameraFragment newInstance(String param1, String param2) {
-        CameraFragment fragment = new CameraFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private PreviewView previewView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        previewView = view.findViewById(R.id.previewView);
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(view.getContext()); //获取cameraProvider
+
+        //检查检查 CameraProvider 可用性
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(view.getContext()));
+
+        return view;
     }
+
+    /**
+     * 选择相机并绑定生命周期和实例
+     * @param cameraProvider
+     */
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview
+                .Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK) //前后镜头设置  0：前镜头 1：后镜头
+                .build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        cameraProvider.unbindAll();  //绑定前解除所有绑定,防止cameraProvider绑定Lifecycle发生异常
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+    }
+
+
 }
--- app/src/main/java/com/util/camerademo/MainActivity.java
+++ app/src/main/java/com/util/camerademo/MainActivity.java
@@ -1,14 +1,63 @@
 package com.util.camerademo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] permissions = new String[]{
+            Manifest.permission.CAMERA   //相机权限
+    };
+    String[] missingPermissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissions(permissions);
+    }
+
+    /**
+     * 相机Fragment
+     */
+    private void startCameraFragment() {
+        CameraFragment mCameraFragment = new CameraFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.frameLayout, mCameraFragment);
+        transaction.commit();
+    }
+
+    /**
+     * 检查应用所需权限
+     *
+     * @param permissions 应用所需权限
+     */
+    private void checkPermissions(String[] permissions) {
+        for (int i = 0; i < permissions.length; i++) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, permissions[i]) == PackageManager.PERMISSION_GRANTED) {
+                startCameraFragment();
+            } else {
+                missingPermissions[i] = permissions[i];
+            }
+        }
+        if (missingPermissions.length != 0) {
+            ActivityCompat.requestPermissions(MainActivity.this, missingPermissions, 200);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
     }
 }
