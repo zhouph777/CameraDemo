6c67dbeb2009f130b67515ae4c327a0516e6b232
zhou.penghui <zhou516371383@163.com>
2021-03-25 16:13:56 +0800

    save file

--- app/src/main/java/com/util/camerademo/CameraFragment.java
+++ app/src/main/java/com/util/camerademo/CameraFragment.java
@@ -1,8 +1,15 @@
 package com.util.camerademo;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
@@ -16,18 +23,14 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 
-import android.util.Log;
-import android.util.Size;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 相机控制Fragment
@@ -53,7 +56,9 @@ public class CameraFragment extends Fragment {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
 
-                bindPreview(view,cameraProvider);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    bindPreview(view,cameraProvider);
+                }
             } catch (ExecutionException | InterruptedException e) {
                 // No errors need to be handled for this Future.
                 // This should never be reached.
@@ -72,6 +77,7 @@ public class CameraFragment extends Fragment {
     ImageAnalysis imageAnalysis;
     ImageCapture imageCapture;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void bindPreview(View view, ProcessCameraProvider cameraProvider) {
         Preview preview = new Preview
                 .Builder()
@@ -83,11 +89,11 @@ public class CameraFragment extends Fragment {
 
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        imageAnalysis =
-                new ImageAnalysis.Builder()
-                        .setTargetResolution(new Size(1280, 720))
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build();
+//        imageAnalysis =
+//                new ImageAnalysis.Builder()
+//                        .setTargetResolution(new Size(1280, 720))
+//                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+//                        .build();
 
         imageCapture =
                 new ImageCapture.Builder()
@@ -95,27 +101,42 @@ public class CameraFragment extends Fragment {
                         .build();
 
         cameraProvider.unbindAll();  //绑定前解除所有绑定,防止cameraProvider绑定Lifecycle发生异常
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, imageAnalysis, imageCapture, preview);
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, imageCapture, preview);
     }
 
-    private void startAn() {
-        imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
-            @Override
-            public void analyze(@NonNull ImageProxy image) {
-                int rotationDegrees = image.getImageInfo().getRotationDegrees();
+//    private void startAn() {
+//        imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
+//            @Override
+//            public void analyze(@NonNull ImageProxy image) {
+//                int rotationDegrees = image.getImageInfo().getRotationDegrees();
+//
+//            }
+//        });
+//    }
 
-            }
-        });
-    }
 
-    Executor executor ;
+    /**
+     * 创建一个线程池
+     * 2个线程
+     * 最大3个线程
+     * 空闲线程等待时间为0
+     * Unit单位
+     * 队列
+     */
+    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+            2, 3, 0,
+            TimeUnit.MILLISECONDS,
+            new LinkedBlockingDeque<>(2));
+
 
     public void takePicture() {
         Log.i("CameraX","保存成功");
         ImageCapture.OutputFileOptions outputFileOptions =
-                new ImageCapture.OutputFileOptions.Builder(new File("test.jpg")).build();
+                new ImageCapture.OutputFileOptions
+                        .Builder(new File("test.jpg"))
+                        .build();
 
-        imageCapture.takePicture(outputFileOptions, executor,
+        imageCapture.takePicture(outputFileOptions, threadPoolExecutor,
                 new ImageCapture.OnImageSavedCallback() {
                     @Override
                     public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
